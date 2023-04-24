5f10009f6706e6beccc6cdcaccb8467b46fea443
narutomst <narutomst@163.com>
2023-04-24 16:08:44 +0800

    1.优化了注释，对主要函数在首次调用时注释了其定义位置
    Signed-off-by: narutomst <narutomst@163.com>

--- 1D-Auto-CNN-Spectral-Classification-Tensorflow2_10_1/global_variable.py
+++ 1D-Auto-CNN-Spectral-Classification-Tensorflow2_10_1/global_variable.py
@@ -7,9 +7,8 @@
 def _init():
     """在主模块初始化"""
     global _global_dict  # 此处为什么要申明为global变量？ 如果不加global，则_global_dict只是一个_init()函数内部的局部变量
-    _global_dict = {'image_file': 'C:\\Matlab练习\\duogun\\PaviaU.mat', 'label_file': 'C:\\Matlab练习\\duogun\\PaviaU_gt.mat', 'num_bands': 103}  
-	# （本来字典的初始化和赋值是分开进行的，Pycharm建议：字典的初始化和赋值可以一起完成）	
-	# 同理，set_value(),get_value()函数内部的_global_dict也都是局部变量，
+    _global_dict = {'image_file': 'D:\\Matlab练习\\duogun\\Pavia.mat', 'label_file': 'D:\\Matlab练习\\duogun\\Pavia_gt.mat', 'num_bands': 102}
+    # 同理，set_value(),get_value()函数内部的_global_dict也都是局部变量，
     # 也就是说，尽管同名，但它们是3个不同的变量，相互之间是完全独立的。因此，申明为global变量的第一个作用是：
     # 在本篇代码文件内，使不同函数能够操作同一个变量，只要变量名是相同的，那么不管出现在本篇的任何地方，它都是同一个变量。
 
